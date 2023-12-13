383ce07c43b48a27a88a2bbd030f68bfdccc269f
Christopher Balcita <chris.x2pher@gmail.com>
2023-12-13 14:21:14 +0800

    initial commit

---
+++ src/main/java/com/coding/recimechallenge/RecimechallengeApplication.java
@@ -0,0 +1,13 @@
+package com.coding.recimechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecimechallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecimechallengeApplication.class, args);
+    }
+
+}
